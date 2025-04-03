f48a33dcf5a866796a6620ca4c19e6f6b9d518ae
Tishana B. <iw547idw@students.codeinstitute.net>
2025-04-03 22:07:28 +0000

    chore: add boilerplate code to index.html style.css and quiz.test.js

--- scripts/quiz.test.js
+++ scripts/quiz.test.js
@@ -2,3 +2,11 @@
  * @jest-environment jsdom
  * jest-environment-options {"url": "https://jest.js.io/"}
  */
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
